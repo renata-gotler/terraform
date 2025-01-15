8f2a076ab2ab55fb44916f1039cb50dffc1b96f7
Renata <84292027+renata-gotler@users.noreply.github.com>
2025-01-15 10:31:01 -0300

    Revert "Add GitHub resources"

--- main.tf
+++ main.tf
@@ -9,28 +9,13 @@ terraform {
       version = "~> 6.0"
     }
   }
-
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
-
 }
 
 provider "github" {
   owner = "renata-gotler" # Troque pela sua conta pessoal ou organização
 }
 
-import {
-  id = "ai_product_template"
-  to = github_repository.this["ai_product_template"]
-}
-
-import {
-  id = "asl-ml-immersion"
-  to = github_repository.this["asl-ml-immersion"]
-}
-
-import {
-  id = "studies"
-  to = github_repository.this["studies"]
-}
+# Adicionar um usuário para a organização
+# resource "github_membership" "membership_for_user_x" {
+   # ...
+# }
