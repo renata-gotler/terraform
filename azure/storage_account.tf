73499a3ecd88c6f14abc8c6440ac6e386a77f35a
Renata Gotler <renatagotler@gmail.com>
2025-01-16 14:59:24 -0300

    add azure

---
+++ azure/storage_account.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "this" {
+  name     = local.rg_name
+  location = local.location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = local.storage_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  is_hns_enabled           = true
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = local.container_name
+  storage_account_id  = azurerm_storage_account.this.id
+  container_access_type = "private"
+}
---
+++ azure/variables.tf
@@ -0,0 +1,9 @@
+locals {
+
+  location = "eastus"
+  rg_name  = "rg-fiap"
+
+  storage_name   = "safiapterraform"
+  container_name = "terraform-state"
+
+}
