f531a7f57f8a08dc6a1a67a222575d66b1d68a0e
Renata Gotler <renatagotler@gmail.com>
2025-01-17 13:17:04 -0300

    fix imports

--- azure/storage_account.tf
+++ azure/storage_account.tf
@@ -9,11 +9,12 @@ resource "azurerm_storage_account" "this" {
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  is_hns_enabled           = true
+  is_hns_enabled           = false
+  allow_nested_items_to_be_public = false
 }
 
 resource "azurerm_storage_container" "this" {
   name                  = local.container_name
-  storage_account_id  = azurerm_storage_account.this.id
   container_access_type = "private"
+  storage_account_name = azurerm_storage_account.this.name
 }
