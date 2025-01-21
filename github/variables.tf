98faff44de4ad8b4ca9510066bf467d21788d11c
Renata <84292027+renata-gotler@users.noreply.github.com>
2025-01-21 20:10:30 -0300

    Update variables.tf

--- github/variables.tf
+++ github/variables.tf
@@ -7,12 +7,17 @@ locals {
   repo_list = [
     { name = "ai_product_template", description = "Template repository for new AI projects", status_checks_main = ["lint", "test"] },
     { name = "ai_project_example", template_repo = "ai_product_template", status_checks_main = ["lint", "test"] },
-    { name = "studies", description = "Template repository for new AI projects" },
-    { name = "asl-ml-immersion", description = "Template repository for new AI projects" },
+    { name = "studies", description = "Template repository for new AI projects" 
+
+
+},
+    { name = "asl-ml-immersion", description = "Template repository for new AI projects" 
+
+
+},
     { name = "terraform", owning_teams = ["Terraform-Admins"], status_checks_main = ["plan / plan"] },
   ]
 
-  repo_list_foreach = { for repo in local.repo_list : repo.name => repo }
 
 }
 
