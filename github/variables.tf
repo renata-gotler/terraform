43323683e36d8e8abc4d74cacd82ad8b329bdf5f
renata-gotler <84292027+renata-gotler@users.noreply.github.com>
2026-03-30 19:57:22 +0000

    terraform-fmt: style formatting.

--- github/variables.tf
+++ github/variables.tf
@@ -7,7 +7,7 @@ locals {
   repo_list = [
     { name = "ai_product_template", description = "Template repository for new AI projects", status_checks_main = ["lint", "test"] },
     { name = "ai_project_example", template_repo = "ai_product_template", status_checks_main = ["lint", "test"] },
-{ name = "ai_test", template_repo = "ai_product_template", status_checks_main = ["lint", "test"] },
+    { name = "ai_test", template_repo = "ai_product_template", status_checks_main = ["lint", "test"] },
     { name = "studies", description = "Template repository for new AI projects" },
     { name = "asl-ml-immersion", description = "Template repository for new AI projects" },
     { name = "terraform", owning_teams = ["Terraform-Admins"], status_checks_main = ["plan / plan"] },
